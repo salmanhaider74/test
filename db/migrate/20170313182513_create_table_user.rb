ac8b09d1334d9bf29c5825f558139ecae3426245
Salman Haider <salman.haider@confiz.com>
2017-03-13 23:50:24 +0500

    first commit

---
+++ db/migrate/20170313182513_create_table_user.rb
@@ -0,0 +1,11 @@
+class CreateTableUser < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string  "username"
+      t.float   "display_name"
+      
+      t.timestamps
+
+    end
+  end
+end
